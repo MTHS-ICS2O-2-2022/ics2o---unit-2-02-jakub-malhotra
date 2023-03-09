433dded4cae32baacce40fce9a08cabfa4e190fd
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-03-09 17:52:05 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: Febuary 2023
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById("add-math").innerHTML = "<p>6 + 5 = " + (6 + 5) + "</p>"
+  document.getElementById("subtract-math").innerHTML = "<p>7 - 3 =  " + (7 - 3) + "</p>"  
+  document.getElementById("multiply-math").innerHTML = "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>" 
+  document.getElementById("divide-math").innerHTML = "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
+  document.getElementById("exponent-math").innerHTML = "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
+}
