bc71bfcfdb7d8c15fd7c6bab127ce46131372a20
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-04-12 16:53:04 +0000

    extra

---
+++ go_app/main.go
@@ -0,0 +1,29 @@
+// Created by: Jakub Malhotra
+// Created on: April 2023
+//
+// This program displays, "area and perimeter of a rectangle"
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function displays, "area and perimeter of a rectangle"
+	// the Go standard is to indent using "tabs" NOT spaces!
+
+var length int
+	fmt.Print("Enter the length: ")
+	fmt.Scan(&length)
+	var width int
+	fmt.Print("Enter the width: ")
+	fmt.Scan(&width)
+area := length * width
+
+perimeter := 2 * length + 2 * width
+
+fmt.Println("The area is:", area, "cm²")
+fmt.Println("The perimeter is:", perimeter, "cm")
+
+	fmt.Println("\nDone.")
+
+}
