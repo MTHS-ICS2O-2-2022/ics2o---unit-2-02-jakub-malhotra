4d3d124967a07b9171b12382c2cac138de50ed80
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-04-12 16:58:12 +0000

    go fix

--- go_app/main.go
+++ go_app/main.go
@@ -2,15 +2,11 @@
 // Created on: April 2023
 //
 // This program displays, "area and perimeter of a rectangle"
-
 package main
-
 import "fmt"
-
 func main() {
 	// This function displays, "area and perimeter of a rectangle"
 	// the Go standard is to indent using "tabs" NOT spaces!
-
 var length int
 	fmt.Print("Enter the length: ")
 	fmt.Scan(&length)
@@ -18,12 +14,8 @@ var length int
 	fmt.Print("Enter the width: ")
 	fmt.Scan(&width)
 area := length * width
-
 perimeter := 2 * length + 2 * width
-
 fmt.Println("The area is:", area, "cm²")
 fmt.Println("The perimeter is:", perimeter, "cm")
-
 	fmt.Println("\nDone.")
-
 }
